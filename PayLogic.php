3c55e7cd4141cfccc429f90abc95bf7dbc32ba21
yanghuanjun <986273747@.com>
2022-06-08 10:15:09 +0800

    微信APP支付

---
+++ PayLogic.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * APP支付(下单与调起支付)
+ */
+
+/** 请填写以下配置信息 */
+$mchid = '';          //微信支付商户号 PartnerID 通过微信支付商户资料审核后邮件发送
+$appid = '';  //微信支付申请对应的公众号的APPID
+$apiKey = '';   //https://pay.weixin.qq.com 帐户设置-安全设置-API安全-API密钥-设置API密钥
+$outTradeNo = '';     //你自己的商品订单号
+$orderName = '';    //订单标题
+$payAmount = '';          //付款金额，单位:元
+$notifyUrl = '';     //付款成功后的回调地址(不要有问号)
+/** 配置结束 */
+$wxPay = new WxpayService($mchid, $appid, $apiKey);
+$wxPay->setTotalFee($payAmount);
+$wxPay->setOutTradeNo($outTradeNo);
+$wxPay->setOrderName($orderName);
+$wxPay->setNotifyUrl($notifyUrl);
+$res = $wxPay->createJsBizPackage();//返回签名和调起支付的参数
+echo json_encode($res);
+exit;
+class WxpayService
+{
+    protected $mchid;
+    protected $appid;
+    protected $apiKey;
+    protected $totalFee;
+    protected $outTradeNo;
+    protected $orderName;
+    protected $notifyUrl;
+    protected $returnUrl;
+    protected $wapUrl;
+    protected $wapName;
+
+    public function __construct($mchid, $appid, $key)
+    {
+        $this->mchid = $mchid;
+        $this->appid = $appid;
+        $this->apiKey = $key;
+    }
+
+    public function setTotalFee($totalFee)
+    {
+        $this->totalFee = $totalFee;
+    }
+
+    public function setOutTradeNo($outTradeNo)
+    {
+        $this->outTradeNo = $outTradeNo;
+    }
+
+    public function setOrderName($orderName)
+    {
+        $this->orderName = $orderName;
+    }
+
+    public function setWapUrl($wapUrl)
+    {
+        $this->wapUrl = $wapUrl;
+    }
+
+    public function setWapName($wapName)
+    {
+        $this->wapName = $wapName;
+    }
+
+    public function setNotifyUrl($notifyUrl)
+    {
+        $this->notifyUrl = $notifyUrl;
+    }
+
+    public function setReturnUrl($returnUrl)
+    {
+        $this->returnUrl = $returnUrl;
+    }
+
+    /**
+     * 发起订单
+     * @return array
+     */
+    public function createJsBizPackage()
+    {
+        $config = array(
+            'mch_id' => $this->mchid,
+            'appid' => $this->appid,
+            'key' => $this->apiKey,
+        );
+        $unified = array(
+            'appid' => $config['appid'],
+            'attach' => 'pay',             //商家数据包，原样返回，如果填写中文，请注意转换为utf-8
+            'body' => $this->orderName,
+            'mch_id' => $config['mch_id'],
+            'nonce_str' => self::createNonceStr(),
+            'notify_url' => $this->notifyUrl,
+            'out_trade_no' => $this->outTradeNo,
+            'spbill_create_ip' => $_SERVER['REMOTE_ADDR'],
+            'total_fee' => floatval($this->totalFee),       //单位分
+            'trade_type' => 'APP'
+        );
+        $unified['sign'] = self::getSign($unified, $config['key']);
+        $responseXml = self::curlPost('https://api.mch.weixin.qq.com/pay/unifiedorder', self::arrayToXml($unified));
+        $unifiedOrder = simplexml_load_string($responseXml, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if ($unifiedOrder->return_code != 'SUCCESS') {
+            throw new \Exception($unifiedOrder->return_msg, -1);
+        }
+        $arr = array(
+            "appid" => $config['appid'],
+            "partnerid" => $config['mch_id'],
+            "prepayid" => (string)$unifiedOrder->prepay_id,
+            "package" => "Sign=WXPay",
+            "noncestr" => self::createNonceStr(),
+            "timestamp" => (string)time()        //这里是字符串的时间戳，不是int，所以需加引号
+        );
+        $arr['sign'] = self::getSign($arr, $config['key']);
+        return $arr;
+
+    }
+
+    public static function curlPost($url = '', $postData = '', $options = array())
+    {
+        if (is_array($postData)) {
+            $postData = http_build_query($postData);
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30); //设置cURL允许执行的最长秒数
+        if (!empty($options)) {
+            curl_setopt_array($ch, $options);
+        }
+        //https请求 不验证证书和host
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+
+    public static function createNonceStr($length = 16)
+    {
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $str = '';
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
+    }
+
+    public static function arrayToXml($arr)
+    {
+        $xml = "<xml>";
+        foreach ($arr as $key => $val) {
+            if (is_numeric($val)) {
+                $xml .= "<" . $key . ">" . $val . "</" . $key . ">";
+            } else
+                $xml .= "<" . $key . "><![CDATA[" . $val . "]]></" . $key . ">";
+        }
+        $xml .= "</xml>";
+        return $xml;
+    }
+
+    /**
+     * 获取签名
+     */
+    public static function getSign($params, $key)
+    {
+        ksort($params, SORT_STRING);
+        $unSignParaString = self::formatQueryParaMap($params, false);
+        $signStr = strtoupper(md5($unSignParaString . "&key=" . $key));
+        return $signStr;
+    }
+
+    protected static function formatQueryParaMap($paraMap, $urlEncode = false)
+    {
+        $buff = "";
+        ksort($paraMap);
+        foreach ($paraMap as $k => $v) {
+            if (null != $v && "null" != $v) {
+                if ($urlEncode) {
+                    $v = urlencode($v);
+                }
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $reqPar = '';
+        if (strlen($buff) > 0) {
+            $reqPar = substr($buff, 0, strlen($buff) - 1);
+        }
+        return $reqPar;
+    }
+}
